903565e3a99326efc1924fff0786ed384d4301b4
uni51 <unieye51@gmail.com>
2024-04-21 13:40:35 +0900

    04 フォームに入力された文字列を状態 (=state) として保持する

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
-import "./App.css";
+import { useState } from "react";
 
 export const App = () => {
+  // 初期値: 空文字列 ''
+  const [text, setText] = useState("");
+
   return (
     <div>
       <form onSubmit={(e) => e.preventDefault()}>
-        <input type="text" value="" onChange={(e) => e.preventDefault()} />
         <input
-          type="submit"
-          value="追加"
-          onSubmit={(e) => e.preventDefault()}
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
         />
+        <input type="submit" />
       </form>
+      {/* ↓ DOM のリアクティブな反応を見るためのサンプル */}
+      <p>{text}</p>
+      {/* ↑ あとで削除 */}
     </div>
   );
 };
