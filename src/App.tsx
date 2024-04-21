3f378352c3f7423c22ded1e2ecac8d937e6ff39b
uni51 <unieye51@gmail.com>
2024-04-21 13:33:08 +0900

    03 Reactコンポーネントの作成

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+export const App = () => {
+  return (
+    <div>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input type="text" value="" onChange={(e) => e.preventDefault()} />
+        <input
+          type="submit"
+          value="追加"
+          onSubmit={(e) => e.preventDefault()}
+        />
+      </form>
+    </div>
+  );
+};
